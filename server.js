a6ca9e1b5e82cff46394e7bdd5cdea23cba5ed37
Kim <jka.d.esperida@gmail.com>
2026-02-19 02:57:33 +0800

    Updates

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ const configuredOrigins = (allowedOrigin || '')
   .filter(Boolean);
 
 const defaultOrigins = [
+  'https://autoconnect-admin-view.vercel.app',
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://localhost:3000',
